7cc848b58509bf11ca157e808dd44fc895deb6d2
eastchrist <east_christ@hotmail.com>
2021-04-20 21:49:30 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -7,10 +7,12 @@ let connection = mysql.createConnection({
   host     : process.env.DATABASE_HOST,
   user     : process.env.DATABASE_USER,
   password : process.env.DATABASE_PASSWORD,
-  database : process.env.DATABASE
+  database : process.env.DATABASE,
+  port: 3307
 });
 
 connection.connect((error) => {
+  console.log("MYSQL Connection...")
   if (error) {
     console.log(error)
   } else {
